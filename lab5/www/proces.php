16ca6813d29f2711a22aa4800957f856a1dad813
ShadowFuri123 <sugaipov.nik1@mail.ru>
2026-03-06 17:08:34 +0200

    Complete step 4

--- lab5/www/proces.php
+++ lab5/www/proces.php
@@ -1,50 +1,16 @@
 <?php
-session_start();
-$username = trim($_POST['username'] ?? '');
-$date_birth = $_POST['date_birth'] ?? '';
-$direction = $_POST['direction'] ?? '';
-$type_help = $_POST['type_help'] ?? '';
-$experience = isset($_POST['experience']) ? 'Есть' : 'Нет';
+require 'db.php';
+require 'Student.php';
 
-$errors = [];
-if (empty($username)) $errors[] = "Имя не должно быть пустым.";
-if (empty($direction)) $errors[] = "Направление не выбрано.";
-if (empty($type_help)) $errors[] = "Вид помощи не выбран.";
+$student = new Student($pdo);
 
-if (!empty($errors)) {
-    $_SESSION['error_message'] = implode(" ", $errors);
-    header("Location: index.php");
-    exit();
-}
+$name = htmlspecialchars($_POST['name']);
+$age = intval($_POST['age']);
+$faculty = htmlspecialchars($_POST['faculty'] ?? '');
+$agree = isset($_POST['agree']) ? 1 : 0;
+$form = $_POST['form'] ?? '';
 
-unset($_SESSION['error_message']);
-$_SESSION['success_message'] = "Данные успешно сохранены!";
+$student->add($name, $age, $faculty, $agree, $form);
 
-$username = htmlspecialchars($username);
-$date_birth = htmlspecialchars($date_birth);
-$direction = htmlspecialchars($direction);
-$type_help = htmlspecialchars($type_help);
-$experience = htmlspecialchars($experience);
-
-$_SESSION['username'] = $username;
-$_SESSION['date_birth'] = $date_birth;
-$_SESSION['direction'] = $direction;
-$_SESSION['type_help'] = $type_help;
-$_SESSION['experience'] = $experience;
-
-
-$line = $username . "," . $date_birth . "," . $direction . "," . $type_help . "," . $experience . "\n";
-file_put_contents("data.txt", $line, FILE_APPEND);
-
-require_once 'ApiClient.php';
-$api = new ApiClient();
-
-$url = 'https://data.gov.ru/opendata/7705846236-blagoorg/data-20200214T0000.json';
-$apiData = $api->request($url);
-
-$_SESSION['api_data'] = $apiData;
-
-setcookie("last_submission", date('Y-m-d H:i:s'), time() + 3600, "/");
 header("Location: index.php");
 exit();
-?>
