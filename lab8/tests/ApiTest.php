8d4e6a9b59d0c65e22a85cad4dd212ce66b6f8e3
ShadowFuri123 <111251397+ShadowFuri123@users.noreply.github.com>
2026-04-14 22:14:32 +0200

    Step 6

--- lab8/tests/ApiTest.php
+++ lab8/tests/ApiTest.php
@@ -1,20 +1,37 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Client;
 
 
 
 class ApiTest extends TestCase
 {
-   public function testRequest()
-   {
-       $client = new Client([
-           'base_uri' => 'http://nginx'
+//     public function testRequest()
+//     {
+//         $client = new Client([
+//             'base_uri' => 'http://nginx'
+//         ]);
+//
+//         $response = $client->get('/index.php');
+//
+//         $this->assertEquals(200, $response->getStatusCode());
+//     }
+
+   public function testMockRequest()
+    {
+       $mock = new MockHandler([
+           new Response(200, [], 'OK')
        ]);
 
-       $response = $client->get('/index.php');
+       $handlerStack = HandlerStack::create($mock);
+       $client = new Client(['handler' => $handlerStack]);
+
+       $response = $client->get('/test');
 
        $this->assertEquals(200, $response->getStatusCode());
-   }
+    }
 }
